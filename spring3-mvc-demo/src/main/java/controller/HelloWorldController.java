f69d06444c0bfb891cc06749664f058690b8f58e
annali <liwen_bj@venustech.com.cn>
2016-05-15 09:28:26 +0800

    modified spring3-mvc-demo

--- spring3-mvc-demo/src/main/java/controller/HelloWorldController.java
+++ spring3-mvc-demo/src/main/java/controller/HelloWorldController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by annali on 5/11/16.
  */
 @Controller
-public class HelloWorldController2 {
+public class HelloWorldController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
---
+++ spring3-mvc-demo/src/main/java/controller/HelloWorldController1.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by annali on 5/13/16.
+ */
+public class HelloWorldController1 implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+}
